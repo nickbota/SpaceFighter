14bd030f853aa26be91c287abe1b4bd52574d564
Nick Bota <nick.bota23@gmail.com>
2025-08-01 14:31:49 +0300

    Player movement fix

--- Assets/_Scripts/Player/PlayerMovement.cs
+++ Assets/_Scripts/Player/PlayerMovement.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement Speed")]
+    [SerializeField] private float movementSpeed = 0.01f;
+
+    [Header("X Limit")]
     [SerializeField] private float boundsX = -5f;
-    [SerializeField] private float sensitivity = 0.01f;
     private Vector3 lastInputPosition;
 
     private void Update()
@@ -14,14 +17,15 @@ public class PlayerMovement : MonoBehaviour
     private void HandleInput()
     {
         if (Input.GetMouseButtonDown(0))
-        {
             lastInputPosition = Input.mousePosition;
+
+        if (Input.GetMouseButton(0))
+        {
             Vector3 delta = Input.mousePosition - lastInputPosition;
-            MovePlayer(delta.x * sensitivity);
+            MovePlayer(delta.x * movementSpeed * Time.deltaTime);
             lastInputPosition = Input.mousePosition;
         }
     }
-
     private void MovePlayer(float deltaX)
     {
         Vector3 pos = transform.position;
